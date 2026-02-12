c8a5226c3ce58da51a0019ee175ccffb25d9dc5a
Rauneet-coder <deaddevil5151@gmail.com>
2026-02-12 16:42:46 +0530

    Updating folder structure

---
+++ Basics/Test-file-03/Create_user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This File is All About how to create variables and pass arguments 
+
+
+read -p "Enter Username: " username
+
+echo "You entered $username"
+
+echo "New User Added"
+
+
+echo "The characters of $0 are $1 & $2"
